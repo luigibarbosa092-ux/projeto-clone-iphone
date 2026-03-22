5ee31a21263d7625a390d8c4c85c3c84fc0172df
luigibarbosa092-ux <luigibarbosa092@gmail.com>
2026-03-22 18:39:59 -0300

    primeiro Commit- projeto iphone

---
+++ scripts.js
@@ -0,0 +1,12 @@
+
+function trocarcor(cor) {
+    let circulo = document.querySelector(".circulo")
+    circulo.style.backgroundColor= cor
+    
+}
+
+function trocaimagem(imagem){
+    let imgiphone = document.querySelector(".iphone")
+    imgiphone.src = imagem
+}
+    
